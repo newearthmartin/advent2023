9922a45a46252e9c37978d1918f9b3ce99aab47e
Martin Massera <martinmassera@gmail.com>
2023-12-13 03:04:25 -0300

    DFS and BFS implementations for day 12

--- 12.py
+++ 12.py
@@ -3,45 +3,61 @@ with open('12.txt') as f:
     lines = [line.strip().split(' ') for line in lines]
     lines = [(record, list(map(int, groups.split(',')))) for record, groups in lines]
 
+def add_char(c, path, p_groups, groups):
+    pos = len(p_groups) - 1
+    if c == '#':
+        if len(path) == 0 or path[-1] == '.':
+            if len(p_groups) >= len(groups): return None
+            if pos >= 0 and p_groups[pos] != groups[pos]: return None
+            p_groups.append(1)
+        else:
+            if p_groups[pos] >= groups[pos]: return None
+            p_groups[-1] += 1
+    path.append(c)
+    return path, p_groups
+
 def get_arrangements(record, groups):
     paths = [([], [])]
-    for i, c in enumerate(record):
+    for c in record:
         new_paths = []
         for path, p_groups in paths:
-            def add_char(c2, new_path, new_p_groups):
-                new_path.append(c2)
-                if c2 == '.':
-                    pos = len(p_groups) - 1
-                    if pos < 0 or (pos < len(groups) and groups[pos] == new_p_groups[pos]): 
-                        new_paths.append((new_path, new_p_groups))
-                elif c2 == '#':
-                    if len(new_path) == 1 or new_path[-2] == '.':
-                        if len(new_p_groups) < len(groups):
-                            new_p_groups.append(1)
-                            new_paths.append((new_path, new_p_groups))
-                    else:
-                        assert len(new_p_groups) > 0
-                        pos = len(p_groups) - 1
-                        new_p_groups[-1] += 1
-                        if new_p_groups[pos] <= groups[pos]:
-                            new_paths.append((new_path, new_p_groups))
-            if c == '?':
-                add_char('#', path.copy(), p_groups.copy())
-                add_char('.', path, p_groups)
+            if c == '.' or c == '#':
+                p1 = add_char(c, path, p_groups, groups)
+                if p1: new_paths.append(p1)
             else:
-                add_char(c, path, p_groups)
+                p1 = add_char('#', path.copy(), p_groups.copy(), groups)
+                p2 = add_char('.', path, p_groups, groups)
+                if p1: new_paths.append(p1)
+                if p2: new_paths.append(p2)
         paths = new_paths
+    return sum(1 for _, p_groups in paths if p_groups == groups)
 
-    def check(path):
-        return path[1] == groups
-    return sum(1 for _ in filter(check, paths))
+def get_arrangements2(record, groups):
+    return get_arrangements_dfs(record, groups, 0, [], [])
+
+def get_arrangements_dfs(record, groups, i, path, p_groups):
+    if i == len(record): return 1 if p_groups == groups else 0
+    c = record[i]
+
+    if c == '.' or c == '#':
+        p1 = add_char(c, path, p_groups, groups)
+        return get_arrangements_dfs(record, groups, i + 1, path, p_groups) if p1 else 0
+    else:
+        path2 = path.copy()
+        p_groups2 = p_groups.copy()
+        p1 = add_char('.', path, p_groups, groups)
+        p2 = add_char('#', path2, p_groups2, groups)
+        rv1 = get_arrangements_dfs(record, groups, i + 1, path, p_groups) if p1 else 0
+        rv2 = get_arrangements_dfs(record, groups, i + 1, path2, p_groups2) if p2 else 0
+        return rv1 + rv2
 
 # for i, (record, groups) in enumerate(lines):
 #     lines[i] = (record + '?') * 5, groups * 5
+# lines.sort(key=lambda l: len(l[0].replace('.', '').replace('#','')))
 
 rv = 0
 for record, groups in lines:
     print(record, groups)
     rv += get_arrangements(record, groups)
+    # rv += get_arrangements2(record, groups)
 print(rv)
- 
