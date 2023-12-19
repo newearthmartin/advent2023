fb64953586c4ea8214294e31680d8df6ba67ef76
Martin Massera <martinmassera@gmail.com>
2023-12-19 10:46:38 -0300

    Day 18 - part 2

--- 18.py
+++ 18.py
@@ -2,68 +2,93 @@ with open('18.txt') as f:
     lines = [line.strip().split(' ') for line in f.readlines() if line.strip()]
     lines = [(direction, int(num), color[2:-1]) for direction, num, color in lines]
 
-min_i, max_i = None, None
-min_j, max_j = None, None
+
+def get_min_max():
+    i, j = 0, 0
+    min_i, min_j, max_i, max_j = 0, 0, 0, 0
+    for direction, num, _ in lines:
+        if direction == 'U': i -= num
+        if direction == 'D': i += num
+        if direction == 'L': j -= num
+        if direction == 'R': j += num
+        max_i = max(max_i, i)
+        max_j = max(max_j, j)
+        min_i = min(min_i, i)
+        min_j = min(min_j, j)
+    return min_i, min_j, max_i, max_j
 
 
 def get_plan():
-    plan = {(0, 0): True}
-    i = 0
-    j = 0
+    min_i, min_j, max_i, max_j = get_min_max()
+    print('creating plan', max_i - min_i, 'rows', max_j - min_j, 'cols')
+    print('initializing')
+    plan = [None] * (max_i - min_i + 1)
+    for i in range(len(plan)):
+        plan[i] = []
+    i, j = 0, 0
+    print('painting')
     for direction, num, _ in lines:
-        def paint(incr_i, incr_j):
-            nonlocal i, j
-            global max_i, max_j, min_i, min_j
+        if direction in ['U', 'D']:
+            incr = 1 if direction == 'D' else -1
+            plan[i - min_i].append((j, 1, direction))
             for _ in range(num):
-                i += incr_i
-                j += incr_j
-                plan[(i,j)] = direction
-            max_i = max(max_i, i) if max_i is not None else i
-            max_j = max(max_j, j) if max_j is not None else j
-            min_i = min(min_i, i) if min_i is not None else i
-            min_j = min(min_j, j) if min_j is not None else j
-        if direction == 'U':
-            plan[(i, j)] = 'U'
-            paint(-1, 0)
-        if direction == 'D':
-            plan[(i, j)] = 'D'
-            paint(1, 0)
-        if direction == 'L': paint(0, -1)
-        if direction == 'R': paint(0, 1)
+                i += incr
+                plan[i - min_i].append((j, 1, direction))
+        else:
+            start = (j + 1) if direction == 'R' else (j - num + 1)
+            plan[i - min_i].append((start, num - 1, direction))
+            if direction == 'R':
+                j += num
+            else:
+                j -= num
+    print('sorting')
+    for line in plan:
+        line.sort(key=lambda e: e[0])
     return plan
 
 
 def count(plan):
+    print('counting: ', end='')
     rv = 0
-    for i in range(min_i, max_i + 1):
+    for segments in plan:
         inout = False
         start = None
-
-        l = []
-        for j in range(min_j - 1, max_j + 2):
-            if (i, j) in plan:
-                direction = plan[(i, j)]
-                rv += 1
-                if direction in ['L', 'R']:
-                    pass
-                elif direction in ['U', 'D']:
-                    if start:
-                        if direction != start:
-                            inout = not inout
-                        start = None
-                    else:
+        last_j = None
+        for j, length, direction in segments:
+            if last_j is not None:
+                assert last_j <= j
+                if last_j < j: start = None
+                if inout: rv += j - last_j
+            last_j = j + length
+            rv += length
+            if direction in ['U', 'D']:
+                if start:
+                    if direction != start:
                         inout = not inout
-                        start = direction
-                l.append(direction)
-            else:
-                start = None
-                if inout:
-                    rv += 1
-                    l.append('#')
+                    start = None
                 else:
-                    l.append(' ')
-        # print(''.join(l))
-    return rv
+                    inout = not inout
+                    start = direction
+    print(rv)
 
 
-print('Part 1:', count(get_plan()))
+def reinterpret():
+    for i, line in enumerate(lines):
+        direction, num, color = line
+        direction = color[-1]
+        if direction == '0': direction = 'R'
+        if direction == '1': direction = 'D'
+        if direction == '2': direction = 'L'
+        if direction == '3': direction = 'U'
+        num = int(color[0:5], base=16)
+        lines[i] = (direction, num, color)
+
+
+print('Part 1\n')
+count(get_plan())
+print()
+
+print('Part 2\n')
+reinterpret()
+count(get_plan())
+
