de8ded7a886e3b3fd195eb038207ccbc1ed80354
Martin Massera <martinmassera@gmail.com>
2023-12-27 01:51:38 -0300

    Day 21 - part 2

--- 21.py
+++ 21.py
@@ -1,6 +1,3 @@
-import json
-
-
 def read_data():
     with (open('21.txt') as f):
         rv = [list(line.strip()) for line in f.readlines()]
@@ -11,11 +8,11 @@ def read_data():
         for line in rv:
             for j, c in enumerate(line):
                 line[j] = c != '#'
-        return rv, si, sj
+        return rv, (si, sj)
 
 
-lines, si, sj = read_data()
-s_pos = si, sj
+lines, s_pos = read_data()
+si, sj = s_pos
 len_lines = len(lines)
 len_line = len(lines[0])
 
@@ -37,99 +34,85 @@ def advance(times, i, j, valid_func):
     return heads
 
 
+def print_lines(heads):
+    for i, j in heads:
+        lines[i][j] = '*'
+    for i, line in enumerate(lines):
+        l = []
+        for j, empty in enumerate(line):
+            if (i, j) in heads:
+                c = '*'
+            else:
+                c = '_' if empty else '#'
+            l.append(c)
+        print(''.join(l))
+    print()
+
+
+def is_valid1(i, j): return 0 <= i < len_lines and 0 <= j < len_line and lines[i][j]
+def is_valid2(i, j): return lines[i % len_lines][j % len_line]
+
+
 def part1():
-    def is_valid(i, j):
-        return 0 <= i < len_lines and 0 <= j < len_line and lines[i][j]
-    heads = advance(64, si, sj, is_valid)
+    heads = advance(64, si, sj, is_valid1)
     return len(heads)
 
 
-def add_caches(heads_cache, curr, prev1, prev2):
-    print('Growing cache', curr, f'as {prev1} + {prev2}')
-    heads_cache[curr] = {}
-    for pos, heads in heads_cache[prev1].items():
-        pi, pj = pos
-        heads_cache[curr][pos] = {(hi + hi2, hj + hj2)
-                                  for hi, hj in heads
-                                  for hi2, hj2 in heads_cache[prev2][((pi + hi) % len_lines, (pj + hj)% len_line)]}
+def filter_range(heads, i0, j0, i1, j1):
+    return [(i, j) for i, j in heads if i0 <= i < i1 and j0 <= j < j1]
 
 
-def get_cache(heads_cache, level, is_valid, prev1=None):
-    if level in heads_cache:
-        print('Already cached', level)
-        return
-    print('Creating cache', level)
-    if level == 1:
-        heads_cache[1] = {}
-        for i in range(len_lines):
-            for j in range(len_line):
-                heads = advance(1, i, j, is_valid)
-                heads_cache[1][(i, j)] = {(hi - i, hj - j) for hi, hj in heads}
-    else:
-        if not prev1:
-            prev1 = level // 2
-        add_caches(heads_cache, level, prev1, level - prev1)
-    print('writing caches file')
-    write_heads_cache(heads_cache)
+def sq_heads_count(heads, sqi, sqj):
+    return len(filter_range(heads, sqi * len_lines, sqj * len_line, (sqi + 1) * len_lines, (sqj + 1) * len_line))
 
 
-def grow_heads_cache(times, is_valid):
-    heads_cache = read_heads_cache()
-    for l in range(times):
-        get_cache(heads_cache, 2 ** l, is_valid)
-    return heads_cache
+def get_heads_count():
+    heads = advance(65 + 131 * 4, si, sj, is_valid2)
+    e_heads = sq_heads_count(heads, 0, 0)
+    o_heads = sq_heads_count(heads, 0, 1)
+    n_point = sq_heads_count(heads, -4, 0)
+    s_point = sq_heads_count(heads, 4, 0)
+    e_point = sq_heads_count(heads, 0, 4)
+    w_point = sq_heads_count(heads, 0, -4)
+    ne1_heads = sq_heads_count(heads, -1, 3)
+    ne2_heads = sq_heads_count(heads, -2, 3)
+    se1_heads = sq_heads_count(heads, 1, 3)
+    se2_heads = sq_heads_count(heads, 2, 3)
+    nw1_heads = sq_heads_count(heads, -1, -3)
+    nw2_heads = sq_heads_count(heads, -2, -3)
+    sw1_heads = sq_heads_count(heads, 1, -3)
+    sw2_heads = sq_heads_count(heads, 2, -3)
+
+    for i in range(-4, 5):
+        for j in range(-4, 5):
+            sq_heads = sq_heads_count(heads, i, j)
+            print(str(sq_heads).ljust(5), end='')
+        print()
+
+    return (e_heads, o_heads,
+            e_point, w_point, n_point, s_point,
+            ne1_heads, nw1_heads, se1_heads, sw1_heads,
+            ne2_heads, nw2_heads, se2_heads, sw2_heads)
 
 
-def write_heads_cache(heads_cache):
-    with open('21.cache.txt', 'w') as out:
-        for level, cache in heads_cache.items():
-            out_cache = {f'{k[0]},{k[1]}': [list(pos) for pos in v] for k, v in cache.items()}
-            out_cache = json.dumps(out_cache).replace(' ', '')
-            out.write(f"{level}: {out_cache}\n")
+def count(squares, heads_count):
+    (e_heads, o_heads,
+     e_point, w_point, n_point, s_point,
+     ne1_heads, nw1_heads, se1_heads, sw1_heads,
+     ne2_heads, nw2_heads, se2_heads, sw2_heads) = heads_count
 
-
-def read_heads_cache():
-    heads_cache = {}
-    print('Loading caches file')
-    try:
-        with open('21.cache.txt') as f:
-            lines = [line.strip() for line in f.readlines()]
-    except FileNotFoundError:
-        return heads_cache
-    for line in lines:
-        level, cache = line.split(': ', maxsplit=1)
-        level = int(level.strip())
-        cache = json.loads(cache.strip())
-        cache = {tuple(map(int, k.split(','))): [tuple(pos) for pos in v] for k, v in cache.items()}
-        heads_cache[level] = cache
-    return heads_cache
+    return (o_heads * (squares ** 2) + e_heads * ((squares - 1) ** 2) +
+            e_point + w_point + n_point + s_point +
+            (ne1_heads + nw1_heads + se1_heads + sw1_heads) * (squares - 1) +
+            (ne2_heads + nw2_heads + se2_heads + sw2_heads) * squares)
 
 
 def part2():
-    def is_valid(i, j):
-        return lines[i % len_lines][j % len_line] is True
-
-    cache_heads = grow_heads_cache(24, is_valid)
-    exit(0)
-
-    cycle_len = max(len_lines, len_line)
-    first_steps = 26501365 % cycle_len
-    cycles = (26501365 - first_steps) // cycle_len
-    heads_cache = get_heads_cache(cycle_len, is_valid)
-    # heads_cache = read_heads_cache()
-
-    print(f'First {first_steps} steps')
-    heads = advance(first_steps, si, sj, is_valid)
-
-    print('Starting cycles')
-    for step in range(cycles):
-        print(step, len(heads))
-        new_heads = set()
-        for hi, hj in heads:
-            new_heads.update((hi + i, hj + j) for i, j in heads_cache[(hi % len_lines, hj % len_line)])
-        heads = new_heads
-    return len(heads)
+    # heads_count = (7424, 7436, 5601, 5585, 5574, 5612, 6501, 6497, 6524, 6512, 934, 921, 945, 939)
+    heads_count = get_heads_count()
+    squares = (26501365 - 65) // 131
+    return count(squares, heads_count)
 
 
-# print('Part 1:', part1())
+print('Part 1:', part1())
 print('Part 2:', part2())
